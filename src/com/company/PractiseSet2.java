b9c7548230e5063a3e33b78fb3c028e7c99a6436
Karan <karansethia24@gmail.com>
2022-03-16 00:27:55 +0530

    Practise set for strings

---
+++ src/com/company/PractiseSet2.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class PractiseSet2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String strUp = sc.next();
+        System.out.println(strUp.toLowerCase());
+    }
+}
